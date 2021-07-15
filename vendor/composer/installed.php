e0b8896b368fdecdc56ad598beb627bbb12071e6
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-07-16 02:58:10 +0530

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ae7ca5b903faa60c0608ec7f41c5e4f5615c246e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ae7ca5b903faa60c0608ec7f41c5e4f5615c246e',
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v195',
+            'version' => '195.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => '88cf8f624299856ac2ef3c096eaf93a4eb4c94a4',
+            'dev_requirement' => true,
+        ),
+    ),
+);
