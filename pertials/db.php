3240da9c2ea0f01410ab6bd7394127ad19ca5ebd
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-07-18 02:07:20 +0530

    Update db.php

--- pertials/db.php
+++ pertials/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host="localhost";
-$user="root";
-$password="";
-$dbname="phpstore";
+$host="fdb34.awardspace.net";
+$user="3897492_phpstore";
+$password="ZXCzxc123@#";
+$dbname="3897492_phpstore";
 
 
 $connection = mysqli_connect($host,$user,$password,$dbname);
